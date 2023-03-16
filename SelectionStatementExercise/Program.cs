0ef73d45536350f2150a80a9c363253a6c79f6c1
yosefamiami@gmail.com <yosefamiami@gmail.com>
2023-03-16 16:44:05 -0400

    if else exercise1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            Console.WriteLine("Try to guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too High");  
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
         }
     }
 }
